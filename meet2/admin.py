d75c88124d62462a0c77d779f817fc4bb5a35810
Rohit <rohitsanjay64@gmail.com>
2020-03-16 19:04:27 +0530

    updated admin

--- meet2/admin.py
+++ meet2/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(PhoneNumber)
+admin.site.register(Post)
+admin.site.register(Comments)
+admin.site.register(Group)
+admin.site.register(Events)
+admin.site.register(HasEvents)
+admin.site.register(HasPosts)
+admin.site.register(UserInterestedEvents)
+admin.site.register(GroupMembers)
+
