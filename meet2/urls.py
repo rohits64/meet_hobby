076bae8b49991ed8746ecbed74345781467445e3
krritik <ritikburnwal@gmail.com>
2020-03-18 12:23:28 +0530

    correct user model and added views for it

--- meet2/urls.py
+++ meet2/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('profile/', views.index,name='index')
     # path('', views.post_list, name='post_list'),
 ]
--- meet2/views.py
+++ meet2/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
-# Create your views here.
+from . forms import *
+from . models import *
+
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    form = SignUpForm(request.POST)
+    profile_form = ProfileForm(request.POST)
+
+    if form.is_valid() and profile_form.is_valid():
+        user = form.save()
+        user.refresh_from_db()
+
+        user.profile.AddressRoomNo = profile_form.cleaned_data.get('AddressRoomNo')
+        user.profile.AddressHall = profile_form.cleaned_data.get('AddressHall')
+        user.save()
+
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('index')    
+    else:
+        form = SignUpForm()
+        profile_form = ProfileForm()
+    return render(request, 'signup.html', {'form':form, 'profile_form':profile_form})        
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.info(request, "You are now logged in as {username}")
+                return redirect('index')
+            else:
+                messages.error(request, "Invaild username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+    form = AuthenticationForm()
+    return render(request, "login.html", context={"form":form}) 
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully")
+    return redirect('home')
+
+def index(request):
+    user = request.user
+    if user.username and user.is_superuser is False:
+        return render(request, 'index.html')
+    else:
+        messages.warning(request, 'You are not logged in. Please login') 
+        return redirect('home')                               
