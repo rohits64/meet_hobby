18dd41b3f0ab51ffafad1daed45b5b1d239c20c5
Rohit <rohitsanjay64@gmail.com>
2020-03-23 16:50:34 +0530

    added links between posts

--- meet2/urls.py
+++ meet2/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_request, name='login'),
